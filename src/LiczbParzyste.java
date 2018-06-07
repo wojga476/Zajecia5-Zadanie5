dd3bd825e95fbaa25afb0038a8bb40cc8a19663f
wojga476 <wojga476@gmail.com>
2018-06-07 18:29:57 +0200

    wyświetlenie sumy liczb parzystych

---
+++ src/LiczbParzyste.java
@@ -0,0 +1,13 @@
+public class LiczbParzyste {
+
+    public static void main(String[] args) {
+
+        for (int liczba = 0; liczba < 100; liczba += 2) {
+            System.out.println(liczba);
+            int suma = liczba+liczba;
+//            System.out.println(suma);
+        }
+            System.out.println("suma liczb parzystych:");
+
+    }
+}
